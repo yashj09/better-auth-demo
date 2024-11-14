8f46c46ec9f93ae167897c4226768a85f6ec4660
Yash Jain <118172073+yashj09@users.noreply.github.com>
2024-11-15 01:38:52 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,16 +4,16 @@ import { NextResponse } from "next/server";
 export default authMiddleware({
   customRedirect: async (session, request) => {
     const baseURL = request.nextUrl.origin;
-    if (request.nextUrl.pathname === "/" && session) {
+    if (request.nextUrl.pathname === "/login" && session) {
       return NextResponse.redirect(new URL("/session", baseURL));
     }
     if (request.nextUrl.pathname === "/session" && !session) {
-      return NextResponse.redirect(new URL("/", baseURL));
+      return NextResponse.redirect(new URL("/login", baseURL));
     }
     return NextResponse.next();
   },
 });
 
 export const config = {
-  matcher: ["/session", "/"],
+  matcher: ["/session", "/login"],
 };
